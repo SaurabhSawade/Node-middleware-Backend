f356ba6c5a7db24f66770c8865c54b2924cc8337
SaurabhSawade <saurabhsawade6@gmail.com>
2025-06-19 20:22:47 +0530

    port update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = 4000;
 
 // //  CORS setup for Vite dev server at :5173
 app.use(cors(
